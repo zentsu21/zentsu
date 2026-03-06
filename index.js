6b6301aae2f2dc7f24cf360f6424f8112f58de0d
Zentsu_21 <xvixmohamedsaad2010x@gmail.com>
2026-03-06 16:39:58 +0200

    Update status message for Spotify to 'The Lost Butterfly'

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
+const statusMessages = ["The Lost Butterfly", "🎮 Playing VALORANT"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
